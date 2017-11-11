3d09426d18daa389e754330614ec0a1210904a93
Elad Yair <eladyair@gmail.com>
2017-11-11 20:45:42 +0200

    Adding tests and Travis CI to the project

---
+++ tests/index.test.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai'; // Used for unit testing
+import jsdom from 'jsdom'; // Used for thesting dom elements within node env
+import fs from 'fs'; // Using the file system node library
+
+describe('Our first test', () => {
+    it('should pass', () => {
+        expect(true).to.equal(true);
+    });
+});
+
+// Tesing h1 element in index.html
+describe('Testing index.html', () => {
+    it('should say hello', (done) => {
+        const index = fs.readFileSync('./src/index.html', 'utf-8');
+        jsdom.env(index, (err, window) => {
+            const h1 = window.document.getElementsByTagName('h1')[0];
+            expect(h1.innerHTML).to.equal("Hello World!");
+            done();
+            window.close();
+        });
+    });
+});
